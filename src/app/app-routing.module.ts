a78cd8beeb7dd534fb1ea00c3ba849c76d054f95
lwalik <l.walicki1@gmail.com>
2023-04-16 16:06:22 +0200

    feat: martianForm to appRouting added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MartiansListComponent } from './components/martians-list/martians-list.component';
-import { MartiansListComponentModule } from './components/martians-list/martians-list.component-module';
 import { JobListComponent } from './components/job-list/job-list.component';
+import { MartianFormComponent } from './components/martian-form/martian-form.component';
 import { JobListComponentModule } from './components/job-list/job-list.component-module';
+import { MartiansListComponentModule } from './components/martians-list/martians-list.component-module';
+import { MartianFormComponentModule } from './components/martian-form/martian-form.component-module';
+import { MartianServiceModule } from './services/martian.service-module';
+
 const routes: Routes = [
   { path: '', component: MartiansListComponent },
   { path: 'job-list', component: JobListComponent },
+  { path: 'martian-form', component: MartianFormComponent },
 ];
 
 @NgModule({
@@ -14,6 +19,8 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     JobListComponentModule,
     MartiansListComponentModule,
+    MartianFormComponentModule,
+    MartianServiceModule,
   ],
   exports: [RouterModule],
 })
