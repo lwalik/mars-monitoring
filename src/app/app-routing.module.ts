cc9587e29417f4444f9b15223780a549e4407b82
annah <anna.h.domanska@gmail.com>
2023-04-16 15:35:11 +0200

    feat: resources-list added to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResourcesListComponent } from './components/resources-list/resources-list.component';
+import { ResourcesListComponentModule } from './components/resources-list/resources-list.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'resources-list', component: ResourcesListComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), ResourcesListComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
