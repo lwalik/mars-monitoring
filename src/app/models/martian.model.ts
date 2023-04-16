e1431d2766772f9a2a6ccbc4c03ac50de9fdb77e
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-04-16 14:58:39 +0200

    maritan model

---
+++ src/app/models/martian.model.ts
@@ -0,0 +1,10 @@
+export interface MartianModel {
+  readonly id: string;
+  readonly name: string;
+  readonly lastName: string;
+  readonly imageUrl: string;
+  readonly skills: string[];
+  readonly sex: string;
+  readonly experienceInSpace: string;
+  readonly workingState: string;
+}
